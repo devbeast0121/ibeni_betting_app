bd55300b5e15794f44996a3557f6298ca2c13060
devbeast0121 <devbeast3800@gmail.com>
2025-12-01 16:28:13 +0200

    initial capacitor - convert to mobile

---
+++ android/app/src/main/java/com/ibeni/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ibeni.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,13 +2,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.f49cb9fbffa74e91a13959de898a8439',
+  appId: 'com.ibeni.app',
   appName: 'ibeni',
   webDir: 'dist',
-  server: {
-    url: 'https://f49cb9fb-ffa7-4e91-a139-59de898a8439.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   ios: {
     contentInset: 'always',
     backgroundColor: '#ffffff',
